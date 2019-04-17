db4cf8193f3ac9da345cd3e6236dabe60a563ea9
Rowan Smith <theintheredwoods@gmail.com>
2019-04-17 15:19:17 -0500

    conditional render functional

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -6,12 +6,13 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
-        });
+            pool.query(`SELECT "content", "secrecy_level" FROM "secret" WHERE '${req.user.clearance_level}' >= "secrecy_level";`)
+                .then(results => res.send(results.rows))
+                .catch(error => {
+                    console.log('Error making SELECT for secrets:', error);
+                    res.sendStatus(500);
+                });
+            
 });
 
 module.exports = router;
